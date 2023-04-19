ca9632a51915bf7063e46e92e38d6533cb0183d8
YongZhen Low <cephyz@gmail.com>
2023-04-19 11:36:14 +0800

    feat: add theme option for logo visibility

--- src/utils/theme.config.ts
+++ src/utils/theme.config.ts
@@ -4,11 +4,11 @@
  * @see https://github.com/TryGhost/Casper/blob/main/package.json
  */
 interface CasperThemeOptions {
+  navigation_layout: 'Logo on cover' | 'Logo in the middle' | 'Stacked';
   title_font: 'Modern sans-serif' | 'Elegant serif';
   body_font: 'Modern sans-serif' | 'Elegant serif';
   show_publication_cover: boolean;
   header_style: 'Center aligned' | 'Left aligned' | 'Hidden';
-  show_logo_in_navigation: boolean;
   feed_layout: 'Classic' | 'Grid' | 'List';
   color_scheme: 'Light' | 'Dark' | 'Auto';
   post_image_style: 'Wide' | 'Full' | 'Small' | 'Hidden';
@@ -16,11 +16,11 @@ interface CasperThemeOptions {
 }
 
 const defaultOptions: CasperThemeOptions = {
+  navigation_layout: 'Logo on cover',
   title_font: 'Modern sans-serif',
   body_font: 'Elegant serif',
   show_publication_cover: true,
   header_style: 'Center aligned',
-  show_logo_in_navigation: false,
   feed_layout: 'Classic',
   color_scheme: 'Light',
   post_image_style: 'Wide',
@@ -31,10 +31,7 @@ const defaultOptions: CasperThemeOptions = {
  * Configure your theme options here
  */
 const customOptions: Partial<CasperThemeOptions> = {
-  body_font: 'Modern sans-serif',
-  show_publication_cover: false,
-  feed_layout: 'Classic',
-  color_scheme: 'Dark',
+  // color_scheme: 'Dark',
 };
 
 const themeConfig: CasperThemeOptions = { ...defaultOptions, ...customOptions };
